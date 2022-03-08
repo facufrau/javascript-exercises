af5f7d9df39d0142bb466a032cbf2c66db4a42c8
Facundo Frau <facufrau@gmail.com>
2022-03-08 18:44:02 -0300

    Complete exercise 08-Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  if (!numbers) {
+    return 0;
+  } else if (numbers.length === 1){
+    return numbers[0];
+  } else {
+    return numbers.reduce((accumulator, item) => { return accumulator += item}, 0)
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((accumulator, item) => {return accumulator *= item}, 1);
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+  return number ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number === 0 || number === 1) {
+    return 1;
+  } else {
+    return factorial(number - 1) * number;
+  }
 };
 
 // Do not edit below this line
