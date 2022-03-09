e4fe416449ee1e57f432629d9ebac10b2fc93fe7
Facundo Frau <facufrau@gmail.com>
2022-03-09 16:53:45 -0300

    Complete exercise 12 - Find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArray) {
+    const oldest = peopleArray.reduce((previous, next) => {
+        if (!previous.yearOfDeath) {
+            previous["yearOfDeath"] = new Date().getFullYear();
+        }
+        if (!next.yearOfDeath) {
+            next["yearOfDeath"] = new Date().getFullYear();
+        }
 
+        let previousAge = previous.yearOfDeath - previous.yearOfBirth;
+        let nextAge = next.yearOfDeath - next.yearOfBirth;
+
+        if (previousAge < nextAge){
+            return next;
+        } else {
+            return previous;
+        }
+    })
+    return oldest;
 };
 
 // Do not edit below this line
