9832a8f30874fdd331f36599f617ad228e177e7d
Facundo Frau <facufrau@gmail.com>
2022-03-09 16:38:50 -0300

    Complete exercises 9, 10 and 11

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (sentence) {
+    let lowerCaseSentence = sentence.toLowerCase();
+    const forwardsString = Array.from(lowerCaseSentence)
+                            .filter(character => (/[a-z]/).test(character))
+                            .join("");
 
+    const backwardsString = Array.from(lowerCaseSentence).reverse()
+                                .filter(character => (/[a-z]/).test(character))
+                                .join("");
+
+    return forwardsString === backwardsString;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(limitNumber) {
+    let start = 0;
+    let current = 1;
+    let next;
+    limitNumber = parseInt(limitNumber);
+    if (limitNumber < 0){
+        return "OOPS";
+    } else if (limitNumber === 1){
+        return current;
+    } else if (limitNumber === 2) {
+        return current;
+    } else {
+        for(let i = 1; i < limitNumber; i++) {
+            next = current + start;
+            start = current;
+            current = next;
+        }
+        return next;
+    };
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
